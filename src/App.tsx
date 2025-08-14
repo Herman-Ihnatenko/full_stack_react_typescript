d4aa71ebd395c92be6af08aad3a1f1e947e2e1fb
Herman-Ihnatenko <german1514u@gmail.com>
2025-08-14 13:00:09 +0200

    lesson_07

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
-import Homework_06 from "./homeworks/Homework_06/homework_06";
 
+import Homework_06 from "homeworks/Homework_06/Homework_06";
+import Lesson_07 from "lessons/Lesson_07/Lesson_07";
 function App() {
-  return <div className="app">
-  <Homework_06 />
-  </div>;
-
+  return (
+    <div className="app">
+      {/* homeworks */}
+      {/* <Homework_06 /> */}
+      {/* lessons */}
+      <Lesson_07 />
+    </div>
+  );
 }
 
 export default App;
--- src/homeworks/Homework_06/Homework_06.tsx
+++ src/homeworks/Homework_06/Homework_06.tsx
@@ -14,13 +14,10 @@ const cars = [
 Используя map JSX элементов, расположите данные из обьектов в карточках и покажите эти карточки на странице. Стили на ваше усмотрение
 */
 
+import { type Car } from "./types";
+import {v4} from "uuid";
 import "./styles.css";
 
-interface Car {
-  brand: string;
-  price: number;
-  isDiesel: boolean;
-}
 const cars: Car[] = [
   { brand: "BMW", price: 20000, isDiesel: true },
   { brand: "Mercedes", price: 22000, isDiesel: false },
@@ -32,8 +29,8 @@ const cars: Car[] = [
 function Homework_06() {
   return (
     <div className="homework_06_page_wrapper">
-      {cars.map((car, index) => (
-        <div key={index} className="car_card">
+      {cars.map((car: Car) => (
+        <div key={v4()} className="car_card">
           <h2>{car.brand}</h2>
           <p>Price: {car.price}</p>
           <p>Diesel: {car.isDiesel ? "Yes" : "No"}</p>
