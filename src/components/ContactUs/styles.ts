c813cd33365847b4d3608832c5a8eb9a4cd1ee8e
Herman-Ihnatenko <german1514u@gmail.com>
2025-08-28 18:20:52 +0200

    homework_11

---
+++ src/components/ContactUs/styles.ts
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+
+export const ContactUsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 590px;
+  min-height: 450px;
+  max-height: fit-content;
+  padding: 60px;
+  border-radius: 4px;
+  background-color: transparent;
+  gap: 30px;
+`;
+
+export const Title = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  color: rgba(255, 0, 0, 1);
+`;
+
+export const InputsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
